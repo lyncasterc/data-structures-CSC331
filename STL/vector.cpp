3c6641dd10167e47c74205d7aba9038b8f7a89a8
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-09-05 15:07:05 -0400

    go over more vector stuff

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 // sequence container
 
@@ -15,6 +16,11 @@
     // at(), [], front(), back(), data()
 
 
+bool f(int x, int y)
+{
+    return x > y;
+}
+
 int main()
 {
     std::vector<int> arr, arr2;
@@ -22,14 +28,54 @@ int main()
     arr.pop_back(); // removes last item, does not return 
     arr.size(); // returns length of vector
     arr.empty(); // returns true if vector is empty
-    
-    arr2.push_back(8); // {8}
+
+    arr2.push_back(8); 
+    arr2 = {12, 3, 5, 10};
 
     // insert - takes iterator and value
     // inserts value at iterator
     auto it = arr2.begin();
-    arr2.insert(it, 2); // {2, 8}
+    arr2.insert(it, 2);
     std::cout << arr2.at(0); // at - returns element at index
+    
+    // sorts vector - requires algorithm library
+    // O(NlogN)
+    std::sort(arr2.begin(), arr2.end()); 
+    for (auto &&i : arr2)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    //std::find returns an iterator equal to arr2.end() if item is not present
+    // O(N)
+    bool present = (std::find(arr2.begin(), arr2.end(), 3) != arr2.end()); 
+    
+    //std::binary_search - returns true if element is present
+    //vector needs to be sorted. O(NLogN)
+    bool present2 = (std::binary_search(arr2.begin(), arr2.end(), 10));
+
+    std::cout << present2 << std::endl;
+
+    // std::lower_bound returns first iterator >= to val 
+    auto it2 = std::lower_bound(arr2.begin(), arr2.end(), 7);
+
+    //std::upper_bound returns the first iterator strictly > to val
+    // O(NLogN) for sorted vectors
+    auto it3 = std::upper_bound(arr2.begin(), arr2.end(), 10);
+
+    std::cout << *it3 << std::endl; 
+
+    // std::distance returns distance between two iterators.
+    // can be used to find index of item in vector
+    int index =  std::distance(arr2.begin(), it3);
+
+    std::cout << index << std::endl;
+
+    // sorting in reverse order - passing in comparative function
+    std::sort(arr2.begin(), arr2.end(), f);
+    
+
     
     
 }
