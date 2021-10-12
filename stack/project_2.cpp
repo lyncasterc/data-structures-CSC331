b763947c15b0f1067a7b24d441bbb70044a4fe52
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-10-12 01:29:46 -0400

    add input validation functions for project_2

--- stack/project_2.cpp
+++ stack/project_2.cpp
@@ -1,65 +1,56 @@
 #include <iostream>
 #include <string>
-#include <stack>
 #include <cctype>
+#include <algorithm>
+#include <regex>
 #include "ListStack.h"
 
-// returns the precedence of operators in the expression
-int str_prec(char x)
+// Programmer: Stiven Cabrera
+// Purpose: Project 2 Solution - solves an expression in infix notation
+// Input: string
+// Output: double
+
+int str_prec(char x);
+int stack_prec(char x);
+double do_operation(char op, double a, double b);
+double solution(const std::string &str);
+void remove_spaces(std::string &expression);
+bool is_parenthesis_balanced(const std::string &expression);
+bool is_correct_format(const std::string expression);
+bool is_valid_input(const std::string &expression);
+
+int main()
 {
-    if(x == '+' || x == '-')
-    { return 1; }
-
-    else if(x == '*' || x == '/')
-    { return 3; }
-
-    else if(x == '(')
-    { return 8; }
-
-    else if(x == ')')
-    { return 0; }
-
-    return -1;  
-};
+    
+    // std::string exp1 = "16/2+5-11*2"; // -9
+    // std::string exp2 = "3+4*2";       // 11
+    // std::string exp3 = "2*5+8*2";     // 26
+    // std::string exp4 = "-3+4";        // 1
+    // std::string exp5 = "-3+-4";      // -7 
+    // std::string exp6 = "2+-3";          // -1
+    // std::string exp7 = "3*(4-2*(3+7))+6"; // -42
+    // std::string exp8 = "8+4*(10-5/(5+10))-3*7"; // 25.6667
+    // std::string exp9 = "(3*(4-2*(3+7))+6/(5+6)-3*4)+1"; // -58.454545
+    // std::cout << solution(exp9) << std::endl;
 
 
-// returns the precendence of operators once inside the operator stack
-int stack_prec(char x)
-{
-    if(x == '+' || x == '-')
-    { return 2; }
+    // std::string input;
+    // char cont_choice;
+    // std::cout << "========CALCULATOR========\n\n";
 
-    else if(x == '*' || x == '/')
-    { return 4; }
-
-    else if(x == '(')
-    { return 0; }
-
-    return -1; 
-};
+    // while(cont_choice != 'q')
+    // {
+    //     std::cout << "Inputs allowed: 0-9,+,-,*,/. Paranthesis and negative numbers are allowed.\n";
+    //     std::cout << "Enter an expression to be evaluated: ";
+    //     getline(std::cin, input);
+    //     input.erase(std::remove_if(input.begin(), input.end(), ::isspace), input.end());
 
 
-double do_operation(char op, double a, double b)
-{
-    double result;
 
-    switch (op)
-    {
-        case '+':
-            result = a + b;
-            break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        case '/':
-            result = a / b;
-            break;
-    }
+    // }
+    
 
-    return result;
+
 }
 
 double solution(const std::string &str)
@@ -118,43 +109,107 @@ double solution(const std::string &str)
     return nums.pop();
 };
 
-
-int main()
+int str_prec(char x)
 {
+    if(x == '+' || x == '-')
+    { return 1; }
+
+    else if(x == '*' || x == '/')
+    { return 3; }
+
+    else if(x == '(')
+    { return 8; }
+
+    else if(x == ')')
+    { return 0; }
+
+    return -1;  
+};
+
+int stack_prec(char x)
+{
+    if(x == '+' || x == '-')
+    { return 2; }
+
+    else if(x == '*' || x == '/')
+    { return 4; }
+
+    else if(x == '(')
+    { return 0; }
+
+    return -1; 
+};
+
+double do_operation(char op, double a, double b)
+{
+    double result;
+
+    switch (op)
+    {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+    }
+
+    return result;
+}
+
+void remove_spaces(std::string &expression)
+{
+
+    expression.erase(std::remove_if(expression.begin(), 
+                                    expression.end(), ::isspace), 
+                                    expression.end());
+}
+
+bool is_parenthesis_balanced(const std::string &expression)
+{
+    char open = '(';
+    char close = ')';
+    ListStack<char> stack;
+
+    for (int i = 0; expression[i] != '\0'; i++)
+    {
+        if(expression[i] == open)
+        { stack.push(open); }
+
+        else if (expression[i] == close)
+        {
+            if(stack.isEmpty())
+            {
+                return false;
+            }
+            else
+            {
+                stack.pop();
+            }
+
+        }
+    }
     
-    // std::string exp1 = "16/2+5-11*2"; // -9
-    // std::string exp2 = "3+4*2";       // 11
-    // std::string exp3 = "2*5+8*2";     // 26
-    // std::string exp4 = "-3+4";        // 1
-    // std::string exp5 = "-3+-4";      // -7 
-    // std::string exp6 = "2+-3";          // -1
-    // std::string exp7 = "3*(4-2*(3+7))+6"; // -42
-    // std::string exp8 = "8+4*(10-5/(5+10))-3*7"; // 25.6667
-    std::string exp9 = "(3*(4-2*(3+7))+6/(5+6)-3*4)+1"; // -58.454545
-    // std::string space = "2 + - \n3\n";
-    // double a, b;
-    // a = 5;
-    // b = 15;
+    if(!stack.isEmpty())
+    { 
+        return false; 
+    }
 
-    // string.erase(std::remove_if(string.begin(), string.end(), ::isstring), string.end());
+    return true;
+}
 
-    std::cout << solution(exp9) << std::endl;
+bool is_correct_format(const std::string expression)
+{
+    return std::regex_match(expression, std::regex ("^[+\\-*/()\\d]+$"));
+}
 
-
-    // std::string input;
-    // char cont_choice;
-    // std::cout << "========CALCULATOR========\n\n";
-
-    // while(cont_choice != 'q')
-    // {
-    //     std::cout << "Inputs allowed: 0-9,+,-,*,/. Paranthesis and negative numbers are allowed.\n";
-    //     std::cout << "Enter an expression to be evaluated: ";
-    //     getline(std::cin, input);
-    //     input.erase(std::remove_if(input.begin(), input.end(), ::isspace), input.end());
-
-
-
-    // }
-
-    
+bool is_valid_input(const std::string &expression)
+{
+    return is_correct_format(expression) && is_parenthesis_balanced(expression);
 }
